24342966b99fd836874a935a44a168609b564bb5
Ryoma Kai <legknmj@gmail.com>
2017-09-30 17:41:58 +0900

    Update audacity.rb

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -2,7 +2,7 @@ cask 'audacity' do
   version '2.1.3'
   sha256 '088e87f8411b9de043a8e3d84a487224c901f673bc104363774362ae636bbd34'
 
-  url "https://www.fosshub.com/Audacity.html/audacity-macos-#{version}.dmg"
+  url "https://www.fosshub.com/Audacity.html/audacity-macos-#{version}.dmg", useragent: :fake
   name 'Audacity'
   homepage 'http://www.audacityteam.org/'
 
