97d41f45e3e2daebd57175c3638b0e5fd5464221
Ryoma Kai <legknmj@icloud.com>
2019-02-15 22:24:27 +0900

    test

--- concourse.rb
+++ concourse.rb
@@ -5,10 +5,10 @@ class Concourse < Formula
   homepage 'https://concourse-ci.org/'
 
   if OS.mac?
-    url 'https://github.com/concourse/concourse/releases/download/v4.2.2/concourse_darwin_amd64'
+    url 'https://github.com/concourse/concourse/releases/download/v4.2.2/concourse_darwin_amd64', :using => :nounzip
     sha256 'cfd26dbe70d8ba24a7ebbf0ef8b11fe06f7d16bc45b5e8fee39acb85885d1267'
   elsif OS.linux?
-    url 'https://github.com/concourse/concourse/releases/download/v4.2.2/concourse_linux_amd64'
+    url 'https://github.com/concourse/concourse/releases/download/v4.2.2/concourse_linux_amd64', :using => :nounzip
     sha256 '5180903fee6a8fcdf8ba5bd34f270c5c467342f45534692af16f2951181e8749'
   end
 
