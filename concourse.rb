58a87ad3511c6293bdb5d261498ab4769ee1a72d
Ryoma Kai <legknmj@icloud.com>
2019-02-16 00:04:17 +0900

    fix

--- concourse.rb
+++ concourse.rb
@@ -4,6 +4,8 @@ class Concourse < Formula
   desc "simple CI Platform"
   homepage 'https://concourse-ci.org/'
 
+  depends_on "postgres"
+
   if OS.mac?
     url 'https://github.com/concourse/concourse/releases/download/v4.2.2/concourse_darwin_amd64', :using => :nounzip
     sha256 'cfd26dbe70d8ba24a7ebbf0ef8b11fe06f7d16bc45b5e8fee39acb85885d1267'
@@ -41,6 +43,7 @@ class Concourse < Formula
       <array>
         <string>#{opt_bin}/concourse</string>
         <string>quickstart</string>
+        <string>--add-local-user=user:pass</string>
         <string>--main-team-allow-all-users</string>
         <string>--postgres-database=postgres</string>
         <string>--worker-work-dir=#{var}/concourse</string>
