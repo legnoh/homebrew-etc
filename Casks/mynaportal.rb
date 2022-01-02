a4eb69148bafc27d7cd48b81105385ea7149793c
Ryoma Kai <legknmj@icloud.com>
2022-01-02 21:23:30 +0900

    rename

---
+++ Casks/mynaportal.rb
@@ -0,0 +1,16 @@
+cask 'mynaportal' do
+    version :latest
+    sha256 :no_check
+  
+    url 'https://img.myna.go.jp/tools/mac/MynaPortalAppSetup.dmg'
+    name 'MynaPortal'
+    name 'Electronic identity card software for Japan MyNumber'
+    name 'eID Japan MyNumberPortal Client'
+    homepage 'https://myna.go.jp/'
+  
+    depends_on macos: '>= :el_capitan'
+  
+    pkg 'MynaPortalAppSetup.pkg'
+  
+    uninstall pkgutil: 'jp.go.cao.mpa'
+end
