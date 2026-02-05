da894674cc824b7928e75942ce9590343340f4d7
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2026-02-05 09:37:18 +0900

    Update dell-u4025qw-firmware-update-utility.rb

--- Casks/dell-u4025qw-firmware-update-utility.rb
+++ Casks/dell-u4025qw-firmware-update-utility.rb
@@ -2,10 +2,10 @@ cask "dell-u4025qw-firmware-update-utility" do
     version "M3T105"
     sha256 "007b5447d1da8f063dd0f8367c2ba761e3e8b61e41ba50fcb52ae74946d2e645"
   
-    url "https://dl.dell.com/FOLDER12227702M/1/Dell_U4025QW_FWUpdate_#{version}_Mac.pkg"
+    url "https://dl.dell.com/FOLDER13750555M/1/Dell_U4025QW_FWUpdate_#{version}_Mac.pkg"
     name "Firmware Update Package for U4025QW monitor."
     desc "Firmware Update Package for U4025QW monitor."
-    homepage "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=0j0yh&oscode=mac1&productcode=u4025qw-monitor&lwp=rt"
+    homepage "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=r7c3k&oscode=mac1&productcode=u4025qw-monitor"
   
     pkg "Dell_U4025QW_FWUpdate_#{version}_Mac.pkg"
   
